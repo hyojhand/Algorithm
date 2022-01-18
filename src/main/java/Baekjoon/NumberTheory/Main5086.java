0f28280d16748ea00076d7252d322f4cc875eb28
hyojhand <sonjw5128@gmail.com>
2022-01-18 22:14:34 +0900

    [Add] Main5086

---
+++ src/main/java/Baekjoon/NumberTheory/Main5086.java
@@ -0,0 +1,37 @@
+package Baekjoon.NumberTheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main5086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if(A == 0 && B == 0) {
+                break;
+            }
+
+            if(A % B == 0) {
+                sb.append("multiple").append('\n');
+            }
+            else if(B % A == 0){
+                sb.append("factor").append('\n');
+            }
+            else {
+                sb.append("neither").append('\n');
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
