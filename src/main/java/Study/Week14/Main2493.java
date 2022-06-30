cbe0c2f57f690c2fcd929ca42af8a1f0354fdbf0
hyojhand <sonjw5128@gmail.com>
2022-07-01 01:27:31 +0900

    [Add] 탑

---
+++ src/main/java/Study/Week14/Main2493.java
@@ -0,0 +1,47 @@
+package Study.Week14;
+
+import java.io.*;
+import java.util.*;
+// 탑
+public class Main2493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        int[] result = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<int[]> stack = new Stack<>();
+
+        for(int i = 0; i < N; i++) {
+            while(!stack.isEmpty()) {
+                if(stack.peek()[0] <= arr[i]) {
+                    stack.pop();
+                }
+                else {
+                    result[i] = stack.peek()[1];
+                    stack.push(new int[] {arr[i],i+1});
+                    break;
+                }
+            }
+
+            if(stack.isEmpty()) {
+                stack.push(new int[] {arr[i],i+1});
+                result[i] = 0;
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+}
